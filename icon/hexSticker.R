a0286afc3ad5cb833aab44413c641902233c2f9f
Shixiang Wang <wangshx@csu.edu.cn>
2024-12-05 16:11:52 +0800

    update icon

--- icon/hexSticker.R
+++ icon/hexSticker.R
@@ -3,9 +3,9 @@
 library(hexSticker)
 
 sticker("icon/christmas.png", package="blit", 
-        p_size=20, s_x=0.9, s_y=.8, s_width=.6, p_y = 1.5,
+        p_size=20, s_x=0.95, s_y=0.83, s_width=.6, p_y = 1.5,
         h_fill="#fceeb9", h_color="#5f7c4b", p_color = "#5f7c4b",
-        spotlight = TRUE, l_x = 0.8, l_y = 1, l_alpha = 0.2,
+        spotlight = TRUE, l_x = 0.8, l_y = 1, l_alpha = 0.1,
         url = "https://github.com/WangLabCSU/blit", u_size = 4, u_color = "#aaa",
         filename="man/figures/logo.png")
 
