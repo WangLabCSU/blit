b53e36cafe59e69acd27b163eac31972f513aa69
wbq <2982433346@qq.com>
2025-06-07 20:19:15 +0800

    Made modifications

--- R/cmd-bedtools.R
+++ R/cmd-bedtools.R
@@ -2,12 +2,10 @@
 #'
 #' The `bedtools` is a powerful toolset for genome arithmetic.
 #' @param subcmd Sub-Command of bedtools. Details see: `r rd_help("bedtools")`.
-#' @param file1 Path to bed/gff(gtf)/vcf/bam(sam) file.
-#' @param file2 Path to the second file, some commands require.
 #' @param ... `r rd_dots("bedtools")`.
-#' @param ofile Path to the output file.
 #' @param bedtools `r rd_cmd("bedtools")`.
 #' @seealso
+#' - <http://bedtools.readthedocs.io/>
 #' - <https://github.com/arq5x/bedtools2/>
 #'
 #' `r rd_seealso()`
@@ -16,24 +14,10 @@
 #' @export
 bedtools <- make_command(
   "bedtools",
-  function(
-    subcmd = NULL,
-    file1,
-    ofile,
-    ...,
-    file2 = NULL,
-    bedtools = NULL
-  ) {
+  function(subcmd = NULL, ..., bedtools = NULL) {
     assert_string(subcmd, allow_empty = FALSE, allow_null = TRUE)
     assert_string(bedtools, allow_empty = FALSE, allow_null = TRUE)
-    BEDTools$new(
-      cmd = bedtools,
-      ...,
-      subcmd = subcmd,
-      file1 = file1,
-      file2 = file2,
-      ofile = ofile
-    )
+    BEDTools$new(cmd = bedtools, ..., subcmd = subcmd)
   }
 )
 
@@ -42,18 +26,13 @@ BEDTools <- R6Class(
   inherit = Command,
   private = list(
     alias = function() "bedtools",
-    setup_help_params = function() "help",
-    combine_params = function(subcmd, file1, file2, ofile) {
-      c(
-        if (private$help) {
-          c(super$combine_params(), subcmd)
-        } else {
-          c(subcmd, super$combine_params())
-        },
-        arg0("-a", file1),
-        if (!is.null(file2)) arg0("-b", file2) else NULL,
-        arg0("-o", ofile),
-      )
+    setup_help_params = function() "--help",
+    combine_params = function(subcmd) {
+      if (private$help) {
+        c(super$combine_params(), subcmd)
+      } else {
+        c(subcmd, super$combine_params())
+      }
     }
   )
 )
