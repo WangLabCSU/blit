7e652cf8877e67addd49d9b6576b586c4c2a94f2
roc-002 <3507755728@qq.com>
2025-06-13 15:05:37 +0800

    add test

--- tests/testthat/test-bwa.R
+++ tests/testthat/test-bwa.R
@@ -1,3 +1,7 @@
-testthat::test_that("`BlitProcess` stdin works well", {
+testthat::test_that("`bwa()` works as expected", {
+  testthat::skip_if_not(nzchar(Sys.which("bwa")))  # 确保系统安装了BWA
+  bwa() |> cmd_help()
 
+  bwa("index") |> cmd_help()
+  bwa("mem") |> cmd_help()
 })
