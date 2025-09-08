5685498c6f169118bf7841089f48abf220859fd8
丁加 <jiading682@qq.com>
2025-09-08 13:12:14 +0800

    Update test-exec.R

--- tests/testthat/test-exec.R
+++ tests/testthat/test-exec.R
@@ -6,7 +6,6 @@ testthat::test_that("`allele_counter()` works as expected", {
 testthat::test_that("`bwa()` works as expected", {
   testthat::skip_if_not(nzchar(Sys.which("bwa")))
   bwa() |> cmd_help()
-
   bwa("index") |> cmd_help()
   bwa("mem") |> cmd_help()
 })
